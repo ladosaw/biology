f46e4f7b3bdf7e393de1e32095216158b308004a
Mark Joseph <josephladosaw@gmail.com>
2024-12-27 21:42:49 +0800

    [Feat]: add interactable Digestive System model

--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -1,7 +1,24 @@
 import * as React from "react";
+import Render3D from "../../components/renderer/Render3d";
+import DigestiveSystem from "../../components/model/DigestiveSystem";
 
 const Lessons = ({ topic }) => {
-  return <div>{topic}</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      {topic === "Digestive System 1" ? (
+        <div className="w-full h-screen">
+          <h1 className="text-4xl text-center font-bold">{topic}</h1>
+          <Render3D>
+            <DigestiveSystem />
+          </Render3D>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">{topic}</h1>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Lessons;
