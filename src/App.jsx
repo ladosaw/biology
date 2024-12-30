1f47ffbf4a121b9bf6232283b69619ca887c4bcf
Mark Joseph <josephladosaw@gmail.com>
2024-12-31 00:18:00 +0800

    [Refactor]: Update App component structure to include AppWrapper and conditionally render Footer based on pathname

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import * as React from "react";
 import { Container, Box } from "@mui/material";
-
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Router from "./routes/index";
-
 import Navbar from "./components/nav-section/NavBar";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
+  const location = useLocation();
+  console.log(location);
   return (
-    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
-      <Box
-        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-      >
-        <Navbar />
-        <Container maxWidth="xl" sx={{ flex: 1 }}>
-          <Box className="my-4">
-            <Router />
-          </Box>
-        </Container>
-        <Footer />
-      </Box>
-    </BrowserRouter>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Navbar />
+      <Container maxWidth="xl" sx={{ flex: 1 }}>
+        <Box className="my-4">
+          <Router />
+        </Box>
+      </Container>
+      {location.pathname !== "/lessons" && <Footer />}
+    </Box>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+    <App />
+  </BrowserRouter>
+);
+
+export default AppWrapper;
