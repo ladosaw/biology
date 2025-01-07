155027cbce26d39e6501e9bd8750a4eea8b351f2
Mark Joseph <josephladosaw@gmail.com>
2025-01-07 22:07:53 +0800

    Update @react-three/drei to version 9.120.5 and add @pmndrs/assets for enhanced rendering capabilities

--- src/components/renderer/Render3d.jsx
+++ src/components/renderer/Render3d.jsx
@@ -1,12 +1,15 @@
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
+import { suspend } from "suspend-react";
+
+const city = import("@pmndrs/assets/hdri/city.exr");
 
 function Render3D({ children }) {
   return (
     <Canvas>
       <Suspense fallback={<>Loading...</>}>{children}</Suspense>
-      <Environment preset="city" />
+      <Environment files={suspend(city).default} />
       <OrbitControls enableZoom enablePan />
       <ContactShadows position={[0, -2, 0]} scale={50} blur={2} far={4.5} />
     </Canvas>
